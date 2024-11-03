4723a7006d7565b115c3cbbb47ea913a82f6b46a
hoshikwyw <khaingwutyiwin1712@gmail.com>
2024-11-03 20:46:59 +0630

    Landing page frame done

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import "../assets/js/main"
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div>
+      <nav className=''>
+        <a href="#">Oro Wee</a>
+        <a href="#">About</a>
+        <a href="#">Projects</a>
+        <a href="#">Contact</a>
+        <button>Flip Tiles</button>
+      </nav>
+      <div className="board" id='board'></div>
+      <div className="blockContainer" id='blockContainer'>
+        <div className="blocks" id='blocksWrapper'></div>
+      </div>
+    </div>
   )
 }
 
--- src/route/Path.jsx
+++ src/route/Path.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import { Routes } from 'react-router-dom'
-import Main from '../pages/Main'
+// import Main from '../pages/Main'
 import Projects from '../pages/Projects'
 import About from '../pages/About'
 import Contact from '../pages/Contact'
+import TileBoard from '../pages/TileBoard'
 
 const Path = () => {
   return (
-    <div>
+    <div className=''>
       <Routes>
-        <Route path='/' element={<Main />} />
+        <Route path='/' element={<TileBoard />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/about' element={<About />} />
